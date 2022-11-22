6f0b195a958aab4063f84b64143d19a31b981d48
prince479 <ps441158@gmail.com>
2022-11-22 15:41:15 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvVariablesV4._8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var appsettings = ConfigurationManager.AppSettings;
+            ViewBag.SMTP_URL = appsettings["SMTP_URL"];
+            ViewBag.SMTP_EMAIL = appsettings["SMTP_EMAIL"];
+            ViewBag.URI = appsettings["uri"];
+            ViewBag.Secret = appsettings["ACOM--DSN--AstrologyDotCom"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnvVariablesV4._8.MvcApplication" Language="C#" %>
